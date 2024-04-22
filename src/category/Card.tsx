36ef6793badc9230af128075b542b9c681399278
Adnen Rebai <adnen.rebai@gmail.com>
2024-04-22 03:36:57 +0200

    feat : add component doc

---
+++ src/category/Card.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from "react";
+
+export interface ContentItem extends ReactElement {
+  component: React.ComponentType<any>;
+  props: { [key: string]: any };
+  label: string;
+  style?: { [key: string]: any };
+}
+interface CardProps {
+  title: string;
+  content: ContentItem[];
+}
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div className="card">
+      <h3> {title}</h3>
+      {content.map((item) => (
+        <item.component key={item.key} style={item.style} {...item.props}>
+          {item.label}
+        </item.component>
+      ))}
+    </div>
+  );
+};
+
+export default Card;
