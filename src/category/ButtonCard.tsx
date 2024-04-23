7ec4df7b0c2ce73e955e7e4542928da303e0fbfe
Adnen Rebai <adnen.rebai@gmail.com>
2024-04-23 23:23:52 +0200

    refactor

--- src/category/ButtonCard.tsx
+++ src/category/ButtonCard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card, { ContentItem } from "./Card";
 import { Button } from "react-novice-button";
 import base from "../theme";
+import { IconCheck } from "@tabler/icons-react";
 
 interface ButtonCardProps {
   title: string;
@@ -32,32 +33,18 @@ export const ButtonShape: React.FC<ButtonCardProps> = ({
 
   return (
     <div className="button-shape-container">
-      <Card title={title} content={content} />
+      <Card title={title} content={content} shape />
       <div className="card card-container ten-item-grid">
         {colors.map((item, index) => (
           <Button
-            className={`grid-item ${
-              selected === index ? "grid-item-selected" : ""
-            }`}
+            className="grid-item"
             key={item}
             // @ts-ignore comment
             $variant={item}
             $size="xs"
             onClick={updateVariant(item, index)}
           >
-            <svg
-              viewBox="0 0 10 7"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                d="M4 4.586L1.707 2.293A1 1 0 1 0 .293 3.707l3 3a.997.997 0 0 0 1.414 0l5-5A1 1 0 1 0 8.293.293L4 4.586z"
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            {selected === index ? <IconCheck height={18} width={18} /> : ""}
           </Button>
         ))}
       </div>
--- src/category/Card.tsx
+++ src/category/Card.tsx
@@ -1,62 +1,173 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
-export interface ContentItem extends ReactElement {
+export interface ContentItem extends React.ReactElement {
   component: React.ComponentType<any>;
   props: { [key: string]: any };
   label: string;
   style?: { [key: string]: any };
+  icon?: React.ReactElement;
 }
 interface CardProps {
   title: string;
   content: ContentItem[];
+  shape?: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const Card: React.FC<CardProps> = ({ title, content, shape }) => {
+  if (shape) {
+    return (
+      <div className="card-container">
+        <div className="card ">
+          <h3> {title}</h3>
+          <div className="card-content">
+            {content.map((item) => (
+              <item.component key={item.key} style={item.style} {...item.props}>
+                {item.label} {item?.icon}
+              </item.component>
+            ))}
+          </div>
+
+          <div className="card-content">
+            {content.map((item) => (
+              <item.component
+                key={item.key}
+                style={item.style}
+                {...item.props}
+                $shape="round-sm"
+              >
+                {item.label} {item?.icon}
+              </item.component>
+            ))}
+          </div>
+
+          <div className="card-content">
+            {content.map((item) => (
+              <item.component
+                key={item.key}
+                style={item.style}
+                {...item.props}
+                $shape="round"
+              >
+                {item.label} {item?.icon}
+              </item.component>
+            ))}
+          </div>
+
+          <div className="card-content">
+            {content.map((item) => (
+              <item.component
+                key={item.key}
+                style={item.style}
+                {...item.props}
+                $shape="pill"
+              >
+                {item.label} {item?.icon}
+              </item.component>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="card-container">
       <div className="card ">
         <h3> {title}</h3>
-        {content.map((item) => (
-          <item.component key={item.key} style={item.style} {...item.props}>
-            {item.label}
-          </item.component>
-        ))}
-      </div>
-      <div className="card">
-        {content.map((item) => (
-          <item.component
-            key={item.key}
-            style={item.style}
-            {...item.props}
-            $shape="round-sm"
-          >
-            {item.label}
-          </item.component>
-        ))}
-      </div>
-      <div className="card">
-        {content.map((item) => (
-          <item.component
-            key={item.key}
-            style={item.style}
-            {...item.props}
-            $shape="round"
-          >
-            {item.label}
-          </item.component>
-        ))}
-      </div>
-      <div className="card">
-        {content.map((item) => (
-          <item.component
-            key={item.key}
-            style={item.style}
-            {...item.props}
-            $shape="pill"
-          >
-            {item.label}
-          </item.component>
-        ))}
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component key={item.key} style={item.style} {...item.props}>
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $outline
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="round-sm"
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="round-sm"
+              $outline
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="round"
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="round"
+              $outline
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="pill"
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
+        <div className="card-content">
+          {content.map((item) => (
+            <item.component
+              key={item.key}
+              style={item.style}
+              {...item.props}
+              $shape="pill"
+              $outline
+            >
+              {item.label} {item?.icon}
+            </item.component>
+          ))}
+        </div>
       </div>
     </div>
   );
